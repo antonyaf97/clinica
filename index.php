d71eeda1bf38c1f66a7761474a17ecf9189eb96e
antonyaf97 <68721174+antonyaf97@users.noreply.github.com>
2020-11-29 13:48:53 -0600

    test

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ require_once "modelos/usuarios.modelo.php";
 require_once "modelos/pacientes.modelo.php";
 require_once "modelos/consultas.modelo.php";
 
+
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
