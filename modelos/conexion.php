8adb481e2cc6e40853881f9fbbfe3a22b3df7fee
antonyaf97 <68721174+antonyaf97@users.noreply.github.com>
2020-11-29 16:12:47 -0600

    test db

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,10 +5,10 @@ class Conexion{
 	static public function conectar(){
 
 		
-		$link = new PDO("mysql:host=MYSQL5030.site4now.net;
-						dbname=db_a6a122_clinica",
-			            "a6a122_clinica",
-			            "clinica001");
+		$link = new PDO("mysql://rr08lv87rc64t5ri:mhd1y63345czzy05@r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lw08u1soapoi7x6j;
+						dbname=lw08u1soapoi7x6j",
+			            "rr08lv87rc64t5ri",
+			            "mhd1y63345czzy05");
 
 		$link ->exec("set name utf8");
 
