d09c9048b2a7745fb7fcbe39427f43a72f8d94fd
antonyaf97 <68721174+antonyaf97@users.noreply.github.com>
2020-11-29 13:38:20 -0600

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,10 +4,11 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;
-						dbname=clinica",
-			            "root",
-			            "");
+		
+		$link = new PDO("mysql:host=MYSQL5030.site4now.net;
+						dbname=db_a6a122_clinica",
+			            "a6a122_clinica",
+			            "clinica001");
 
 		$link ->exec("set name utf8");
 
